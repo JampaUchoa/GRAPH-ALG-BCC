3e81be3d2a2fbd0e3189e92bbe8eec9fd65d95ba
Jampa Uchoa <uchoa@motorola.com>
2019-04-08 09:44:28 -0300

    Update main

--- main.c
+++ main.c
@@ -1,40 +1,41 @@
 #include <stdio.h>
 #include "GRAPH_LST.h"
+#include "GRAPH_MTX.h"
 
 int main(void) {
 
-   Graph g = GRAPHinit(8);
+   M_Graph g = M_GRAPHinit(8);
 
-   GRAPHinsertArc(g, 0, 4);
-   GRAPHinsertArc(g, 4, 1);
-   GRAPHinsertArc(g, 0, 3);
-   GRAPHinsertArc(g, 3, 4);
-   GRAPHinsertArc(g, 2, 3);
-   GRAPHinsertArc(g, 3, 5);
-   GRAPHinsertArc(g, 5, 2);
-   GRAPHinsertArc(g, 5, 7);
-   GRAPHinsertArc(g, 7, 6);
-   GRAPHinsertArc(g, 6, 5);
-   GRAPHinsertArc(g, 5, 6);
-   GRAPHinsertArc(g, 3, 1);
+   M_GRAPHinsertArc(g, 0, 4);
+   M_GRAPHinsertArc(g, 4, 1);
+   M_GRAPHinsertArc(g, 0, 3);
+   M_GRAPHinsertArc(g, 3, 4);
+   M_GRAPHinsertArc(g, 2, 3);
+   M_GRAPHinsertArc(g, 3, 5);
+   M_GRAPHinsertArc(g, 5, 2);
+   M_GRAPHinsertArc(g, 5, 7);
+   M_GRAPHinsertArc(g, 7, 6);
+   M_GRAPHinsertArc(g, 6, 5);
+   M_GRAPHinsertArc(g, 5, 6);
+   M_GRAPHinsertArc(g, 3, 1);
 
 
-   GRAPHshow(g);
-   int *v = GRAPH_isSource2(g);
+   M_GRAPHshow(g);
+   int *v = M_GRAPH_isSource(g);
    
-   GRAPHremoveArc(g,3,5);
-   GRAPHremoveArc(g,4,1);
-   //GRAPHshow(g);
-   //vertex *v = GRAPH_isSource(g);
+   M_GRAPHremoveArc(g,3,5);
+   M_GRAPHremoveArc(g,4,1);
+   //M_GRAPHshow(g);
+   //vertex *v = M_GRAPH_isSource(g);
 
    for(int i = 0; i < g->V; i++)
       printf("%d ", v[i]);
 
 
   printf("\n");
-  // Graph h = GRAPHcomplement(g);
+  // Graph h = M_GRAPHcomplement(g);
 
-  // GRAPHshow(h);
+  // M_GRAPHshow(h);
 
 
   return 0;
